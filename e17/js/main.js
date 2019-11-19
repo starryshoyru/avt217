2cda57bd68a12e25f440e9bc41d25da21febb28b
malhar <msiddik2@gmu.edu>
2019-11-18 21:43:37 -0500

    e17

--- e17/js/main.js
+++ e17/js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
   console.log('THE DOCUMENT IS READY! DO SOMETHING AWESOME!');
 
-  $( '.firstweather' ).click(function() {
+  $( '.degrees75' ).click(function() {
     $('.number1').toggleClass('changecolor');
   });
   $( '.thunderstorm' ).click(function() {
     $('.number2').toggleClass('shrinktext');
   });
-  $( '.snow' ).click(function() {
-    $('.number3').toggleClass('align-left');
+  $( '.snowstorm' ).click(function() {
+    $('.number3').toggleClass('align-right');
   });
 });
